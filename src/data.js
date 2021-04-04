1f6515eda3d0b095d6e6034ae0d94f87cbaa9837
dontknow462 <72941938+dontknow462@users.noreply.github.com>
2021-04-04 20:14:36 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,55 @@
+
+
+const data = [
+  {
+    name: 'Coffee',
+    price: '£ 2.35 ',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG',
+    text:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+  },
+  {
+    name: 'Hamburger',
+    price: '£ 0.89',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1024px-RedDot_Burger.jpg',
+    text:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    name: 'Pizza ',
+    price: '£ 4.99',
+    image:
+      'https://images.squaremeal.co.uk/cloud/article/9339/images/pizza-express-favourite-casual-dining-brand_18092019094334.jpg?w=1000',
+    text:
+      "it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+  },
+  {
+    name: 'Fish and chips',
+    price: '£ 4.09',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Fish_and_chips_blackpool.jpg/1024px-Fish_and_chips_blackpool.jpg',
+    text:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+  },
+  {
+    name: 'Sushi',
+    price: '£ 9',
+    image:
+      'https://images.japancentre.com/recipes/pics/18/main/makisushi.jpg?1557308201',
+    text:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.",
+  },
+  {
+    name: 'Momo',
+    price: '£ 5',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/a/a1/Momo_nepal.jpg',
+    text:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+  },
+ 
+];
+
+export default data;
---
+++ src/index.js
@@ -0,0 +1,38 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+
+// ReactDOM.render(
+//   <>
+//     <App />
+
+//   </>,
+//   document.getElementById('root')
+// );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import { AppProvider } from './context';
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <AppProvider>
+//       <App />
+//     </AppProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+ import './index.css';
+import App from './App';
+ReactDOM.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
